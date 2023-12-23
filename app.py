15d62a22c259be8479ac41791d88f70aff995c52
Roan Paulus <roanpaulus@outlook.com>
2023-12-23 18:33:03 +0100

    Refactored globals and floating functions into classes, coupling data and logic that was already coupled

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
 """Run with run.sh for the needed enviroment variables"""
 import os
-import uuid
 
-from flask import Flask, request, render_template, session, abort
+from flask import Flask, request, render_template, session
 from flask_socketio import SocketIO, send, join_room, leave_room
 import requests
+
+from chat import Chat
 
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 socketio = SocketIO(app, logger=True, engineio_logger=True)
 
-chat_rooms = []
-
-
-class BiChatRoom:
-    def __init__(self) -> None:
-        self.id: str = str(uuid.uuid4())
-        self.users: list[str] = []
-        self.full: bool = False
-
-    def add_user(self, user: str) -> None:
-        if not self.full:
-            self.users.append(user)
-            if len(self.users) == 2:
-                self.full = True
-
-    def __str__(self):
-        return f"BiChatRoom:{str(self.id)}"
+chat = Chat()
 
 
 @socketio.on("connect")
@@ -45,19 +30,12 @@ def handle_message(data):
     send(f"{session["username"]}: {data["message"]}", to=session["room"])
 
 
-def lookup_room() -> object:
-    for room in chat_rooms:
-        if not room.full:
-            return room
-
-
 @socketio.on("join")
 def handle_join(data):
-    room = lookup_room()
+    room = chat.get_room()
     if room is None:
-        room = BiChatRoom()
+        room = chat.add_room(slots=2)
         room.add_user(request.sid)
-        chat_rooms.append(room)
     else:
         room.add_user(request.sid)
 
@@ -74,12 +52,11 @@ def handle_join(data):
 
 @socketio.on("leave")
 def handle_leave(data):
-    print("bye bye")
     send(f"{request.sid} has left the chat.", to=session["room"])
 
 
 @socketio.on('disconnect')
-def handle_disconnect(data):
+def handle_disconnect():
     send(f"{session["username"]} has disconnected", to=session["room"])
 
 
@@ -89,7 +66,7 @@ def index():
 
 
 @app.route("/chat", methods=["GET", "POST"])
-def chat():
+def chatt():
     return render_template("chat.html")
 
 
---
+++ chat.py
@@ -0,0 +1,46 @@
+from uuid import uuid4
+
+
+class Chat:
+    def __init__(self):
+        self.rooms: list[object] = []
+
+    def get_room(self) -> object | None:
+        """Returns a room that is not full."""
+        for room in self.rooms:
+            if not room.is_full():
+                return room
+
+    def get_user(self, user_sid: str) -> int:
+        """Return the indexes of the room with the user you want to find."""
+        for room_i, room in enumerate(self.rooms):
+            user_i = room.users.index(user_sid)
+            if user_i != -1:
+                return room_i, user_i
+
+    def add_room(self, slots: int) -> None:
+        """Add an empty chat room to chat rooms then return the room."""
+        room = Room(slots)
+        self.rooms.append(room)
+        return room
+
+
+class Room:
+    def __init__(self, slots: int) -> None:
+        self.id: str = str(uuid4())
+        self.users: list[str] = []
+
+        if slots < 2:
+            raise ValueError("A minimum of two slots is required.")
+        self.slots = slots
+
+    def add_user(self, user: str) -> None:
+        if not self.is_full():
+            self.users.append(user)
+
+    def is_full(self):
+        return len(self.users) >= self.slots
+
+    def __str__(self):
+        return f"ChatRoom:{str(self.id)}"
+
